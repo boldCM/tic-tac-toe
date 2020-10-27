5baa4cc6c78a0ef20e33a7b0a781f110b0e58ca6
boldCM <carolin@cc-meier.de>
2020-10-27 10:26:32 +0100

    Fix placing of functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,9 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handeClick(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
     const newSquares = squares.slice();
     newSquares[index] = xIsNext ? "😎" : "🧐";
     setSquares(newSquares);
@@ -17,8 +20,14 @@ export default function Board() {
     return <Square value={squares[index]} onClick={() => handeClick(index)} />;
   }
 
-  const status = "Next player: " + (xIsNext ? "😎" : "🧐");
-
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "😎" : "🧐");
+  }
+  // const status = "Next player: " + (xIsNext ? "😎" : "🧐");
   return (
     <div>
       <div className="status">{status}</div>
@@ -39,4 +48,24 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
